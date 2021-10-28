17fa50a9a83ef8ca19d13fcdbd04b2c9c3d557ad
JOELTG12 <joeltomygeorge@gmail.com>
2021-10-28 21:20:37 +0530

    main.java 28-10-2021

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("This is the first java program in Learning Java.");
+        System.out.println("this is on 28-10-2021");
     }
 }
